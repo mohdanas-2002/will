efaba4b54329796686e6043411878b9ce4c7c990
mohdanas-2002 <1manas7396@gmail.com>
2024-12-09 06:49:17 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,17 @@
+class Demo
+
+{
+	public static void main(...)
+	{
+		s.o.print("Hello World")
+		s.o.print("Hello World")
+		s.o.print("Hello World")
+		s.o.print("Hello World")
+	}
+}
+
+
+
+
+
+
